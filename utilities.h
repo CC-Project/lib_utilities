3f22dcc3457143cb57e7e9568262f8f971c09631
herbertz0 <howherbertzo@gmail.com>
2015-05-18 18:59:01 +0200

    Crap

--- utilities.h
+++ utilities.h
@@ -21,16 +21,9 @@
 
     #ifdef DEBUG
         void print_var_bits(uint8_t var);
-<<<<<<< HEAD
         void error(char* str); // Exit the program with the error code EXIT_FAILED
     #endif
 
-=======
-
-    #endif
-    void error(char* str); // Exit the program with the error code EXIT_FAILED
-
->>>>>>> origin/master
     uint16_t int_pow(uint16_t a, uint16_t b);   // Computes a^b
     uint8_t opposite_bit(uint8_t bit);
 #endif // UTILITIES_H
